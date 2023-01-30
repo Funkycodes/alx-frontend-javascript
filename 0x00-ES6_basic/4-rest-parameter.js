84eb2b7f14ca60be505bed1c06602d8157e93a1c
theMaskedOtaku <otakus3nnin@gmail.com>
2023-01-30 14:38:15 +0100

    Riddim

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,4 @@
 export default function returnHowManyArguments(...rest) {
   return rest.length;
 }
+
--- 0x00-ES6_basic/8-getBudgetForCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetForCurrentYear.js
@@ -16,3 +16,4 @@ export default function getBudgetForCurrentYear(income, gdp, capita) {
 
   return budget;
 }
+
